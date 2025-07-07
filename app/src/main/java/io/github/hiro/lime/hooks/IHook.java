2704f02b5505696a88b388ec9b4eeae524d59753
topsuplove1122 <83127934+topsuplove1122@users.noreply.github.com>
2025-07-07 21:24:16 +0800

    Delete app/src/main/java/io/github/hiro/lime directory

--- app/src/main/java/io/github/hiro/lime/hooks/IHook.java
+++
@@ -1,8 +0,0 @@
-package io.github.hiro.lime.hooks;
-
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import io.github.hiro.lime.LimeOptions;
-
-public interface IHook {
-    void hook(LimeOptions limeOptions, XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable;
-}
