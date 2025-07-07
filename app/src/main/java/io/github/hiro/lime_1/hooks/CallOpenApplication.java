c2e1237d1638a2a13baf78b52868462640df5572
topsuplove1122 <83127934+topsuplove1122@users.noreply.github.com>
2025-07-07 21:29:54 +0800

    Update CallOpenApplication.java

--- app/src/main/java/io/github/hiro/lime_1/hooks/CallOpenApplication.java
+++ app/src/main/java/io/github/hiro/lime_1/hooks/CallOpenApplication.java
@@ -1,4 +1,4 @@
-package io.github.hiro.lime.hooks;
+package io.github.hiro.lime_1.hooks;
 
 
 
@@ -23,7 +23,7 @@ import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import io.github.hiro.lime.LimeOptions;
+import io.github.hiro.lime_1.LimeOptions;
 
 public class CallOpenApplication implements IHook {
     @Override
@@ -71,7 +71,7 @@ public class CallOpenApplication implements IHook {
         button.setLayoutParams(params);
 
         button.setOnClickListener(v -> {
-            Intent intent = activity.getPackageManager().getLaunchIntentForPackage("jp.naver.line.android");
+            Intent intent = activity.getPackageManager().getLaunchIntentForPackage("jp.naver.line1.android");
             if (intent != null) {
                 activity.startActivity(intent);
             } else {
