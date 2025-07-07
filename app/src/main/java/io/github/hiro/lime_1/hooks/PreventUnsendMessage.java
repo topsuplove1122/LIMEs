c61e569219591594e003c0a6aac2e722bbe47bae
topsuplove1122 <83127934+topsuplove1122@users.noreply.github.com>
2025-07-07 21:39:31 +0800

    Update PreventUnsendMessage.java

--- app/src/main/java/io/github/hiro/lime_1/hooks/PreventUnsendMessage.java
+++ app/src/main/java/io/github/hiro/lime_1/hooks/PreventUnsendMessage.java
@@ -1,4 +1,4 @@
-package io.github.hiro.lime.hooks;
+package io.github.hiro.lime_1.hooks;
 
 import java.lang.reflect.Field;
 import java.util.ArrayList;
@@ -7,7 +7,7 @@ import java.util.Map;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import io.github.hiro.lime.LimeOptions;
+import io.github.hiro.lime_1.LimeOptions;
 
 public class PreventUnsendMessage implements IHook {
     @Override
