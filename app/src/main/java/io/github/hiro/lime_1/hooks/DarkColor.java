403a4ed3d045a02d94e0e093b63567777837f199
topsuplove1122 <83127934+topsuplove1122@users.noreply.github.com>
2025-07-07 21:32:57 +0800

    Update DarkColor.java

--- app/src/main/java/io/github/hiro/lime_1/hooks/DarkColor.java
+++ app/src/main/java/io/github/hiro/lime_1/hooks/DarkColor.java
@@ -1,6 +1,6 @@
-package io.github.hiro.lime.hooks;
+package io.github.hiro.lime_1.hooks;
 
-import static io.github.hiro.lime.Main.limeOptions;
+import static io.github.hiro.lime_1.Main.limeOptions;
 
 import android.app.Activity;
 import android.content.res.Configuration;
@@ -26,7 +26,7 @@ import java.util.Set;
 import de.robv.android.xposed.XC_MethodHook;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
-import io.github.hiro.lime.LimeOptions;
+import io.github.hiro.lime_1.LimeOptions;
 
 public class DarkColor implements IHook {
     @Override
