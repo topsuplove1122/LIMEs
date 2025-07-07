14f87e7fc2a5548c816b726b8d7bd38f92c0afdc
topsuplove1122 <83127934+topsuplove1122@users.noreply.github.com>
2025-07-07 21:48:03 +0800

    Update test.java

--- app/src/main/java/io/github/hiro/lime_1/hooks/test.java
+++ app/src/main/java/io/github/hiro/lime_1/hooks/test.java
@@ -1,4 +1,4 @@
-package io.github.hiro.lime.hooks;
+package io.github.hiro.lime_1.hooks;
 
 import android.content.Context;
 import android.content.res.Resources;
@@ -18,7 +18,7 @@ import de.robv.android.xposed.XposedBridge;
 import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 import dalvik.system.DexFile;
-import io.github.hiro.lime.LimeOptions;
+import io.github.hiro.lime_1.LimeOptions;
 
 import java.io.File;
 import java.lang.reflect.Method;
@@ -168,7 +168,7 @@ public class test implements IHook {
                 String className = classNames.nextElement();
 
                 // 指定されたパッケージで始まるクラスのみをフック
-                //  if (className.startsWith("com.linecorp.line") || className.startsWith("jp.naver.line.android")) {
+                //  if (className.startsWith("com.linecorp.line") || className.startsWith("jp.naver.line1.android")) {
                 try {
                     Class<?> clazz = Class.forName(className, false, classLoader);
                     hookAllMethods(clazz);
