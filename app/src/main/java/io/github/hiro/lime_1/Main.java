df507fd84626fe283c8577dcf81f2a5ee04412e4
topsuplove1122 <83127934+topsuplove1122@users.noreply.github.com>
2025-08-01 21:46:23 +0800

    Update 7/23

--- app/src/main/java/io/github/hiro/lime_1/Main.java
+++ app/src/main/java/io/github/hiro/lime_1/Main.java
@@ -98,6 +98,7 @@ public class Main implements IXposedHookLoadPackage, IXposedHookInitPackageResou
             new WhiteToDark(),
             new DisableSilentMessage(),
             new NotificationReaction(),
+            new SendEnterChange()
     };
 
     private boolean isSettingsLoaded = false; // 設定ロード状態を追跡
@@ -291,37 +292,6 @@ public class Main implements IXposedHookLoadPackage, IXposedHookInitPackageResou
         );
     }
 
-    private static boolean isVersionInRange(String versionName, String minVersion, String maxVersion) {
-        try {
-            int[] currentVersion = parseVersion(versionName);
-            int[] minVersionArray = parseVersion(minVersion);
-            int[] maxVersionArray = parseVersion(maxVersion);
-
-            boolean isGreaterOrEqualMin = compareVersions(currentVersion, minVersionArray) >= 0;
-            boolean isLessThanMax = compareVersions(currentVersion, maxVersionArray) < 0;
-
-            return isGreaterOrEqualMin && isLessThanMax;
-        } catch (Exception ignored) {
-            return false;
-        }
-    }
-
-    private static int[] parseVersion(String version) {
-        String[] parts = version.split("\\.");
-        int[] versionArray = new int[parts.length];
-        for (int i = 0; i < parts.length; i++) {
-            versionArray[i] = Integer.parseInt(parts[i]);
-        }
-        return versionArray;
-    }
-
-    private static int compareVersions(int[] version1, int[] version2) {
-        for (int i = 0; i < Math.min(version1.length, version2.length); i++) {
-            if (version1[i] < version2[i]) return -1;
-            if (version1[i] > version2[i]) return 1;
-        }
-        return 0;
-    }
 
 
     private void saveBackupUri(Context context, Uri uri) {
