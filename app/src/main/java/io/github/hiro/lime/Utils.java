62028120de39ac8db0b9e33f5651b980753b5405
topsuplove1122 <83127934+topsuplove1122@users.noreply.github.com>
2026-03-31 18:24:59 +0800

    Update Utils.java

--- app/src/main/java/io/github/hiro/lime/Utils.java
+++ app/src/main/java/io/github/hiro/lime/Utils.java
@@ -1,19 +1,6 @@
 package io.github.hiro.lime;
-
 import android.content.Context;
-import android.content.res.AssetManager;
-
-import java.lang.reflect.Method;
-
 public class Utils {
-    public static int dpToPx(int dp, Context context) {
-        float density = context.getResources().getDisplayMetrics().density;
-        return Math.round((float) dp * density);
-    }
-
-    public static void addModuleAssetPath(Context context) throws Throwable {
-        Method mAddAddAssertPath = AssetManager.class.getDeclaredMethod("addAssetPath", String.class);
-        mAddAddAssertPath.setAccessible(true);
-        mAddAddAssertPath.invoke(context.getResources().getAssets(), Main.modulePath);
-    }
+    // 暫時留空，避免其他檔案引用出錯
+    public static void addAssetPath(Context context) {}
 }
