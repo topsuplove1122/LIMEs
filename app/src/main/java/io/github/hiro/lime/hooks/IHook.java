6427313abda28f1c0c0f227b89685383fb14ba25
topsuplove1122 <83127934+topsuplove1122@users.noreply.github.com>
2026-03-31 17:03:17 +0800

    Update IHook.java

--- app/src/main/java/io/github/hiro/lime/hooks/IHook.java
+++ app/src/main/java/io/github/hiro/lime/hooks/IHook.java
@@ -1,8 +1,14 @@
 package io.github.hiro.lime.hooks;
 
-import de.robv.android.xposed.callbacks.XC_LoadPackage;
+// 1. 移除舊版的 de.robv.android.xposed.callbacks.XC_LoadPackage 匯入
+import io.github.hiro.lime.LimeModule;
 import io.github.hiro.lime.LimeOptions;
 
 public interface IHook {
-    void hook(LimeOptions limeOptions, XC_LoadPackage.LoadPackageParam loadPackageParam) throws Throwable;
+    /**
+     * @param module      傳入你的 LimeModule 實體，用來呼叫 module.hook() 與 module.log()
+     * @param classLoader 用來取代舊版 param.classLoader，負責尋找 LINE 的 Class
+     * @param limeOptions 你的設定檔，用來判斷開關狀態
+     */
+    void hook(LimeModule module, ClassLoader classLoader, LimeOptions limeOptions) throws Throwable;
 }
