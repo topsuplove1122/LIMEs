d6a454f5fd3cb89904ae9c66eb7427005ca55faf
topsuplove1122 <83127934+topsuplove1122@users.noreply.github.com>
2026-04-01 01:11:30 +0800

    Update and rename Utils.java to Utils.kt

--- app/src/main/java/io/github/hiro/lime/Utils.java
+++
@@ -1,6 +0,0 @@
-package io.github.hiro.lime;
-import android.content.Context;
-public class Utils {
-    // 暫時留空，避免其他檔案引用出錯
-    public static void addAssetPath(Context context) {}
-}
---
+++ app/src/main/java/io/github/hiro/lime/Utils.kt
@@ -0,0 +1,15 @@
+package io.github.hiro.lime
+
+import android.content.Context
+
+object Utils {
+    /**
+     * 暫時留空，避免其他檔案引用出錯。
+     * 在 Java 中調用方式為：Utils.addAssetPath(context);
+     * 在 Kotlin 中調用方式為：Utils.addAssetPath(context)
+     */
+    @JvmStatic
+    fun addAssetPath(context: Context) {
+        // 待實作
+    }
+}
