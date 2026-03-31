123d527024e30f9f4aa72092917b046c45480272
topsuplove1122 <83127934+topsuplove1122@users.noreply.github.com>
2026-03-31 23:33:02 +0800

    Update Constants.java

--- app/src/main/java/io/github/hiro/lime/hooks/Constants.java
+++ app/src/main/java/io/github/hiro/lime/hooks/Constants.java
@@ -1,6 +1,7 @@
 package io.github.hiro.lime.hooks;
 
 import android.content.Context;
+import android.util.Log;
 import io.github.hiro.lime.LimeModule;
 
 public class Constants {
@@ -11,9 +12,12 @@ public class Constants {
     public static HookTarget WEBVIEW_CLIENT_HOOK = new HookTarget("VP0.k", "onPageFinished");
 
     public static void initializeHooks(Context context, LimeModule module) {
+        String versionName = "14.19.1"; // 🪂 預設降落傘，防止 Context 抓不到時出錯
         try {
-            String versionName = context.getPackageManager().getPackageInfo(PACKAGE_NAME, 0).versionName;
-            module.log(2, "LIMEs", "LINE Version: " + versionName);
+            if (context != null) {
+                versionName = context.getPackageManager().getPackageInfo(PACKAGE_NAME, 0).versionName;
+            }
+            Log.e("LIMEs", "LINE 目標版本: " + versionName);
 
             if (versionName.equals("14.19.1")) {
                 REQUEST_HOOK = new HookTarget("org.apache.thrift.l", "b");
@@ -22,7 +26,7 @@ public class Constants {
                 RESPONSE_HOOK = new HookTarget("org.apache.thrift.l", "a");
             }
         } catch (Exception e) {
-            module.log(4, "LIMEs", "Constants init failed");
+            Log.e("LIMEs", "Constants 初始化錯誤: " + e.getMessage());
         }
     }
 
